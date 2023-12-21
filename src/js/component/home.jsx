957a9ebab4475bdff9cecedfc904be7f7a1b0657
Benjamin Hernandez Morales <147661243+panc4ked@users.noreply.github.com>
2023-12-21 22:16:45 +0000

    todo listo - los iconos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import React from "react";
 
 const Home = () => {
+	const [InputValue, setInputValue] = useState("")
+	const [ToDos, setToDos] = useState([])
 	return (
 		<body>
 			<div className="container">
 				<h1>To-Dos</h1>
 				<ul>
-					<li><input type="text" placeholder="Input task here"/></li>
-					<li>hola<i className="fa-solid fa-x fa-lg" style="color: #000000;"></i></li>
-					<li>hola<i className="fa-solid fa-x fa-lg" style="color: #000000;"></i></li>
-					<li>hola<i className="fa-solid fa-x fa-lg" style="color: #000000;"></i></li>
+					<li><input type="text" placeholder="Input task here" onChange = { (e) => setInputValue(e.target.value)} value={InputValue} onKeyPress={(e) => { if (e.key === "Enter"){ setToDos(ToDos.concat(InputValue)); setInputValue("")}}}/></li>
+					{ToDos.map((a, index) => (
+					<li>{a}<button onClick={ () => setToDos(ToDos.filter((a, currentIndex) => index != currentIndex))}>X</button></li>
+					))}
 				</ul> 
-				<footer>3 tasks left! Keep it up!</footer>
+				<footer>{ToDos.length} tasks left! Keep it up!</footer>
 			</div>
 		</body>
 	);
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.css";
 import Home from "./component/home.jsx";
+/* import "./icons.js"; */
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
